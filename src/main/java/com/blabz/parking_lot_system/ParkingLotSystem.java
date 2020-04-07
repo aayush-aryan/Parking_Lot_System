0c9934f377f15d55ff90bd73cc60693e60f336c5
aayush-aryan <log2aayusharyan@gmail.com>
2020-04-07 12:26:14 +0530

    [Aayush]added. adding welcome message to master branch

--- src/main/java/com/blabz/parking_lot_system/ParkingLotSystem.java
+++ src/main/java/com/blabz/parking_lot_system/ParkingLotSystem.java
@@ -1,23 +1,8 @@
 package com.blabz.parking_lot_system;
 
 public class ParkingLotSystem {
-    private Object vehicle;
 
-    public ParkingLotSystem(){
-    }
-    public boolean park(Object vehicle) {
-        if(this.vehicle!=null){
-            return false;
-        }
-        this.vehicle=vehicle;
-        return true;
-    }
-    public boolean unPark(Object vehicle) {
-        if (this.vehicle == null) return false;
-        if (this.vehicle.equals(vehicle)) {
-            this.vehicle = null;
-            return true;
-        }
-        return false;
+    public static void main(String[] args) {
+        System.out.println(" Welcome to ParkingLotSystem");
     }
 }
